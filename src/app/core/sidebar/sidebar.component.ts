17f2c598ea344e00a66a50bfd589696c180ce989
isaacmayela <isaacmayela27@gmail.com>
2024-07-22 08:15:32 +0200

    feat: sidebar integration

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import ushirikianoLogo from '../../../../public/images/assets/'
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  // ushirikianoLogoPath = ushirikianoLogo;
 
 }
